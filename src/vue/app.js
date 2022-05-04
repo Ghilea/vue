89f5886da54ddf82426b95a2392cfc02e7fa0e77
Dennis Karlsson <tougent@gmail.com>
2022-05-04 16:55:02 +0200

    update

--- src/vue/app.js
+++ src/vue/app.js
@@ -1,7 +1,50 @@
 Vue.createApp({
     data() {
-        return {
-            guest: 'Barn'
+
+        const childrenMenu = {
+            first: {
+                title: 'Smarrig plockmat'
+            },
+            second: {
+                title: 'Barnens favoriter'
+            },
+            third: {
+                title: 'Varmt var det här'
+            }
         }
+
+        const adultMenu = {
+            first: {
+                title: 'Tilltugg & smårättter',
+                dish: [
+                    {
+                        name: 'Friterad kycklingsnacks med chilibearnaise',
+                        describe: 'Deep fried chiken with chilibearnaise'
+                    },
+                    {
+                        name: 'Husets toast skagen',
+                        describe: 'Smörstekt surdegsbröd med räckor, kräftsjärtar, majonnäs, dill & citron toppad med löjrom'
+                    }
+                ]
+            },
+            second: {
+                title: 'Förrätter'
+            },
+            third: {
+                title: 'Varmrätter'
+            }
+        }
+
+        let guestType = 'Vuxen'
+
+        return {
+            title: 'Meny',
+            guest: guestType,
+            menu: {
+                first: (guestType === 'Barn') ? childrenMenu.first.title : (guestType === 'Vuxen') ? adultMenu.first.title : '',
+                second: (guestType === 'Barn') ? childrenMenu.second.title : (guestType === 'Vuxen') ? adultMenu.second.title : ''
+            }
+        }
+        
     }
 }).mount('#app')
