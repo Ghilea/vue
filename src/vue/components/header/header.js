8a6de69db46e9e6fd2e43dc9b3ed4626c62368b3
Dennis <tougent@gmail.com>
2022-05-08 20:23:31 +0200

    update

--- src/vue/components/header/header.js
+++ src/vue/components/header/header.js
@@ -3,18 +3,30 @@ import { store } from '../store';
 export default {
     data() {
         return {
-
+            title: 'Totalsumma:'
         }
     },
     methods: {
-        
+        showTotalCost(){
+            let totalCost = 0;
+            store.state.menu.map((item) => {
+                let newItem = item.dish.filter((dishes) => {
+                    if (dishes.aside > 0) {
+                        totalCost += dishes.price
+                    }
+                    return dishes
+                })
+
+                console.log(newItem)
+            })
+        }
     },
     render() {
-       
+        this.showTotalCost();
         return (
         
             <header class="navbar">
-                <div>Totalsumma: 1</div>
+                <div>{this.title} </div>
             </header>
         
         )
