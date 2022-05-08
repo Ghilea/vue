141d757b4a6dce4d2fd7b06c76d042f3a465ec69
Dennis <tougent@gmail.com>
2022-05-08 19:37:38 +0200

    update

--- src/vue/components/footer/footer.js
+++ src/vue/components/footer/footer.js
@@ -1,33 +1,44 @@
-import { store } from '../store';
-
 export default {
     data() {
         return {
-           
+           contact: {
+                   title: 'Kontakt',
+                   describe: [
+                       'Klarabergsvägen 24, 653 78 Stockholm',
+                       '056-787442',
+                       'blabla'
+                   ]
+               },
+            open: {
+                title: 'Öppttider',
+                describe: [
+                    'Måndag, Tisdag, Onsdag, Torsdag 16.00 - 22.30',
+                    'Fredag 15.00 - 23.00',
+                    'Lördag 13.00 - 23.00',
+                    'Söndag 13.00 - 22.00'
+                ]
+            }
         }
     },
-    methods: {
-        
-    },
     render() {
-       
+        const contactArray = this.contact.describe.map((item) => {
+            return <p>{item}</p>
+        })
+
+        const openArray = this.open.describe.map((item) => {
+            return <p>{item}</p>
+        })
+
         return (
             <footer>
                 <section>
-                    <h2>Kontakt</h2>
-                    <p>Klarabergsvägen 24, 653 78 Stockholm</p>
-                    <p>056-4054050</p>
-                    <p>blabla@blabla.se</p>
-
-
+                    <h2>{this.contact.title}</h2>
+                    {contactArray}
                 </section>
 
                 <section>
-                    <h2>Öppettider</h2>
-                    <p>Måndag, Tisdag, Onsdag, Torsdag 16.00 - 22.30</p>
-                    <p>Fredag 15.00 - 23.00</p>
-                    <p>Lördag 13.00 - 23.00</p>
-                    <p>Söndag 13.00 - 22.00</p>
+                    <h2>{this.open.title}</h2>
+                    {openArray}
                 </section>
             </footer>
         )
