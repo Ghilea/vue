21c2063d1653ed485762a4260bb0d4330332497a
Dennis <tougent@gmail.com>
2022-05-09 20:54:11 +0200

    update

--- src/vue/components/checkout/checkout.js
+++ src/vue/components/checkout/checkout.js
@@ -6,7 +6,16 @@ export default {
             title: ' Kundkorg '
         }
     },
+    methods: {
+        clickOutside(event) {
+            const cart = document.querySelector('.cart');
 
+            if (!cart.contains(event.target)) {
+                console.log(cart.contains(event.target));
+                store.state.showCart = false;
+            }
+        }
+    },
     render() {
 
         const overlay = document.querySelector('.overlay_all');
@@ -15,10 +24,14 @@ export default {
         if (store.state.showCart) {
             body.classList.add('stop');
             overlay.classList.add('overlay');
+
+            overlay.addEventListener('click', this.clickOutside);
+
         } else {
             body.classList.remove('stop');
 
             if (overlay) {
+                overlay.removeEventListener('click', this.clickOutside);
                 overlay.classList.remove('overlay');
             }
             
@@ -32,7 +45,7 @@ export default {
             <h1>{this.title}</h1>
             <p>{store.state.totalCost} kr</p>
             <div>{showOrders}</div>
-            <div class="closeCartButton" onClick={()=>store.state.showCart = false}>
+            <div class="closeCartButton" onClick={()=> store.state.showCart = false}>
             </div>
             </div>
         )
