d46cfc95d948a2956d160a376060af19c85f4646
jojjish <johannab_3@hotmail.com>
2022-05-09 15:43:26 +0100

    UPDATE HEADER

--- src/vue/components/header/header.js
+++ src/vue/components/header/header.js
@@ -3,13 +3,19 @@ import { store } from '../store';
 export default {
     data() {
         return {
-            title: 'Totalsumma: '
+            nameRes:'Resturang D.V.J.A ',
+            title: 'Totalsumma: ',
         }
     },
     render() {
 
         return (
             <header class="navbar">
+                <div class="resname">
+                <i class="fa-solid fa-utensils"></i>
+                 {this.nameRes}
+                 <i class="fa-solid fa-utensils"></i>
+                </div>
                 <div>
                     {this.title}
                     {store.state.totalCost} kr
