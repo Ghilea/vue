62e7908f6646c5c644f85694bcd6c6608653ed75
Dennis Karlsson <tougent@gmail.com>
2022-05-09 13:43:20 +0200

    update

--- src/vue/components/app.js
+++ src/vue/components/app.js
@@ -2,11 +2,12 @@ import Dish_header from './dishes/dish_header';
 import Dish_main from './dishes/dish_main';
 import Header from './header/header';
 import Footer from './footer/footer';
+import { store } from './store';
 
 export default (
     <>
         <Header />
-        <main>
+        <main class={(store.state.menu_selected === 'barn') ? 'childrenMenu' : ''}>
             <Dish_header />
             <Dish_main />
         </main>
