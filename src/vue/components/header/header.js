2354df9b91be1b898ff10f3d429d2c3f78d17d5f
jojjish <johannab_3@hotmail.com>
2022-05-13 13:51:34 +0100

    header

--- src/vue/components/header/header.js
+++ src/vue/components/header/header.js
@@ -3,19 +3,26 @@ import { store } from "../store";
 export default {
     data() {
         return {
-            nameRes:'Resturang D.V.J'
+            nameRes:'Lyxkryssning',
+            img:'https://as2.ftcdn.net/v2/jpg/02/28/13/53/1000_F_228135369_EWQJsS9TEskImi51e5AxpEsYsonOIBMM.jpg',
         }
     },
     render() {
- 
+        const date = new Date()
+        console.log(date)
         return (
             <header class={`navbar ${ (store.state.menu_selected === 'barn') ? 'childpicture' : (store.state.menu_selected === 'par') ? 'couplepicture' : '' }`}>
+                
                 <div class="resname">
-                <i class="fa-solid fa-utensils"></i>
-                 {this.nameRes}
-                 <i class="fa-solid fa-utensils"></i>
+                <img src={this.img}/>
+                {this.nameRes}
+                <div class="date">
+                {date}
                 </div>
+                </div>
+            
             </header>
         )
+       
     }
 };
