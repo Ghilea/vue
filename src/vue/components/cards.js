3c2b677d62d06bd2c5b47317cdcdaa564824ad26
Dennis Karlsson <tougent@gmail.com>
2022-05-16 15:11:58 +0200

    update

--- src/vue/components/cards.js
+++ src/vue/components/cards.js
@@ -6,18 +6,38 @@ export default {
         }
     },
     methods: {
-       
+      
     },
     beforeMount() {
-       
+   
     },
     render() {
-     
         return (
             <div class='cards'>
-                <div>
+                <div class='spa btn'>
+                    <div class='front'>
+                        <h2>Titel</h2>
+                        <div class='frosted'></div>
+                    </div>
+                    <div class='back'>
+                        <div class='img'></div>
+                    </div>
+                    
+                </div>
+                <div class='event btn'>
                     <h2>Titel</h2>
                     <div class='img'></div>
+                    <div class='frosted'></div>
+                </div>
+                <div class='restaurant btn'>
+                    <h2>Titel</h2>
+                    <div class='img'></div>
+                    <div class='frosted'></div>
+                </div>
+                <div class='booking btn'>
+                    <h2>Titel</h2>
+                    <div class='img'></div>
+                    <div class='frosted'></div>
                 </div>
             </div>
         )
