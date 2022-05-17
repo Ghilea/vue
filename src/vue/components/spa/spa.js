32ab6b8908bb29bc8df1ee24c46fbf83bf0e169d
Dennis Karlsson <tougent@gmail.com>
2022-05-17 12:11:41 +0200

    update

--- src/vue/components/spa/spa.js
+++ src/vue/components/spa/spa.js
@@ -5,13 +5,19 @@ export default {
     methods: {
        spaT() {
         const spa = spaTreatments.map((item) => {
-            return(<h1>{item.title}</h1>
-             {item.treatment.map((item2) => {
-                return <p> {item2.name} </p>
-            }}))
-        
-
+            return(
             
+                <div>
+                <h1>{item.title}</h1>
+                
+                {
+                    item.treatment.map((item2) => {
+                        return <p>{item2.name}</p>
+                    })
+                }
+                </div>
+            
+            )  
         })
 
         return spa;
