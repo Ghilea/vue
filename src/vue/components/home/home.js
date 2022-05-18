229b952bfa5b03493063860c4eabc4f854a11d13
Dennis Karlsson <tougent@gmail.com>
2022-05-18 12:07:17 +0200

    update

--- src/vue/components/home/home.js
+++ src/vue/components/home/home.js
@@ -1,15 +1,26 @@
 export default {
     data() {
         return {
-           
+           title: 'The Many Sides of the Caribbean',
+           describe: 'You can’t ask for a better place to get away from it all than the Caribbean. With over 80 islands in the Caribbean, and multiple cultures contributing to their creation, you can be sure no two regions are alike. Chill in the Eastern Caribbean, explore history in the Western Caribbean, marvel at natural wonders in Southern Caribbean. Discover the one-and-only Everglades in Florida, exotic rainforests in Costa Rica, and the never-ending beauty of the ABC Islands.'
         }
     },    
     render() {
  
         return (
-            <>
-                Detta är startsidan
-            </>
+            <div class='home'>
+                <header>
+                    <h2>{this.title}</h2>
+                    <p>{this.describe}</p>
+                </header>
+
+                <div class='container'>
+                    <div class='imgCard'></div>
+                    <div class='imgCard'></div>
+                    <div class='imgCard'></div>
+                    <div class='imgCard'></div>
+                </div>
+            </div>
         )
        
     }
