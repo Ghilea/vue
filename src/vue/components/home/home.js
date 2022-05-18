a701cef40d741fa3de015ad776fb5d584481c391
Dennis Karlsson <tougent@gmail.com>
2022-05-18 13:08:50 +0200

    update

--- src/vue/components/home/home.js
+++ src/vue/components/home/home.js
@@ -1,11 +1,28 @@
 export default {
     data() {
         return {
-           title: 'The Many Sides of the Caribbean',
-           describe: 'You can’t ask for a better place to get away from it all than the Caribbean. With over 80 islands in the Caribbean, and multiple cultures contributing to their creation, you can be sure no two regions are alike. Chill in the Eastern Caribbean, explore history in the Western Caribbean, marvel at natural wonders in Southern Caribbean. Discover the one-and-only Everglades in Florida, exotic rainforests in Costa Rica, and the never-ending beauty of the ABC Islands.'
+            title: 'The Many Sides of the Caribbean',
+            describe: 'You can’t ask for a better place to get away from it all than the Caribbean. With over 80 islands in the Caribbean, and multiple cultures contributing to their creation, you can be sure no two regions are alike. Chill in the Eastern Caribbean, explore history in the Western Caribbean, marvel at natural wonders in Southern Caribbean. Discover the one-and-only Everglades in Florida, exotic rainforests in Costa Rica, and the never-ending beauty of the ABC Islands.',
+            content: [
+                {
+                    title: 'Antigua',
+                    button: 'Explore Antigua',
+                    img: './assets/image/home/antigua.jpg'
+                }
+           ]
         }
     },    
     render() {
+
+        const content = this.content.map((item) => {
+            return (
+                <div class='imgCard'>
+                    <img src={item.img} />
+                    <h2>{item.title}</h2>
+                    <button>{item.button}</button>
+                </div>
+            )
+        })
  
         return (
             <div class='home'>
@@ -15,10 +32,7 @@ export default {
                 </header>
 
                 <div class='container'>
-                    <div class='imgCard'></div>
-                    <div class='imgCard'></div>
-                    <div class='imgCard'></div>
-                    <div class='imgCard'></div>
+                    {content}
                 </div>
             </div>
         )
