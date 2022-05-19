6bfeea409d543fe08b901ef460550134a2bb14ed
annawattman <anna.wattman@student.kyh.se>
2022-05-19 14:47:14 +0200

    :D

--- src/vue/components/event/dataEvent.js
+++ src/vue/components/event/dataEvent.js
@@ -3,20 +3,20 @@ export const events = [{
         hide: false,
         todos: [{
                 name: 'Musikkvällar',
-                describe: '',
+                describe: 'Varje kväll erbjuder vi underhållning av olika artister, däribland Eric Saade, Markoolio, George Washington samt Justin Bieber. Kom och dansa med oss!',
                 time:'Mån-Ons 18:00-22:00',
                 aside: 0,
                 hide: false
             },{
                 name: 'Flamingofredag',
-                describe: '',
+                describe: 'Är ni redo att utforska er relation och hitta spänningen igen? Varmt välkomna på vår "flamingo-friday", en swingerklubb för nybörjare samt mer erfarna swingare. OBS! Åldersgräns: 25+, barnpassning av utbildad personal finns tillgängligt under hela kvällen.',
                 time:'Fre 20:00-03:00',
                 aside: 0,
                 hide: false
             },{
                 name: 'Stora scenen',
-                describe: '',
-                time:'Tors-Lörd 18:00-23:00',
+                describe: 'KOLLA HIT! På stora scenen bjuder vi på roliga uppträdanden av välkända ansikten, utmanande tävlingar med bra priser och en kväll i världsklass! Kommande vecka 23: Onsdag: Anders Bagge planterar blommor i taket och cyklar överbord. Torsdag: Edward Blom dansar naken och lagar falukorv, Fredag: Tony Irving joddlar och pratar ut om hundvalpen Kents humörssvängningar, Lördag: Carola jonglerar med päronskrutt och sjunger hela bibeln baklänges. VÄLKOMNA!',
+                time:'Ons-Lörd 18:00-23:00',
                 aside: 0,
                 hide: false
             },
