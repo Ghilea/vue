b9c9ca383b3635d1797d70a4603a56c91e15fd20
jojjish <johannab_3@hotmail.com>
2022-05-19 14:57:06 +0100

    event

--- src/vue/components/event/dataEvent.js
+++ src/vue/components/event/dataEvent.js
@@ -27,25 +27,25 @@ export const events = [{
         todos: [{
                 name:'Gym',
                 describe:'På kryssningen erbjuder vi våra gäster fritt inträde till vårat stora toppmorderna gym som har en underbar utsikt över havet. Här finns maskiner och fria vikter för allas behov.',
-                time:'Gymmet hittar du på våning 4',
+                time:'Gymmet hittar du på våning 3.',
                 aside: 0,
                 hide: false
             },{
                 name:'Pool',
                 describe:'Vi har flera olika sortes pooler. Stora poolen har en pool inomhus och en ute i solen med solstolar runtomkring. Barnpoolen ligger på våning 1 där det finns samma upplägg fast mer anpassat åt våra yngre besökare.',
-                time:'Stora poolen hittar du på våning 2, Barn poolen på våning 1',
+                time:'Stora poolen hittar du på våning 2, Barn poolen på våning 5.',
                 aside: 0,
                 hide: false
             },{
                 name:'Spa',
                 describe:'Här erbjuder vi flera olika behandlingar som du kan boka i förtid eller på plats. Vi har bubbelpoolar,olika typer av bastu, bubbelpolar och meditations avdelingar.',
-                time:'Spaet hittar du på våning 3',
+                time:'Spaet hittar du på våning 4.',
                 aside: 0,
                 hide: false
             },{
                 name: 'Barer',
                 describe:'Vi har flertal olika baren på fartyget. Ute på soldäcken och inne i olika delar av fartyget. Här erbjuds goda drinkar av topp bartenders till alla våra besökare.',
-                time:'Barerna kan du hitta på våning 1,2 och 3',
+                time:'Barerna kan du hitta på våning 5 och uppåt.',
                 aside: 0,
                 hide: false
             }
