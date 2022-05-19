f701e743de5117830c51b52133eb865ac81ab664
Viktor-Wahlstrom <viktor@wiff.se>
2022-05-19 12:00:15 +0200

    bokning header

--- src/vue/components/booking/booking.js
+++ src/vue/components/booking/booking.js
@@ -3,10 +3,10 @@ import { store } from "../store";
 export default {
     data() {
         return {
-           
+
         }
     },
-    
+
     methods: {
         restaurantBook() {
             const date = document.querySelector('.resDate');
@@ -35,52 +35,57 @@ export default {
 
 
     render() {
- 
+
         return (
-            <div class="booking">
-               <button class="bookButton" onClick={() => store.state.restaurantModal = true}>Boka Resturang</button>
-               <div class="restaurantModal" v-show={store.state.restaurantModal}>
-                   <button class="closeBtn" onClick={() => store.state.restaurantModal = false}></button>
-                   Resturang
-                   <input class="resDate" type="date" placeholder="Vilken dag?"/>
-                   <input class="resTime" type="time" placeholder="Vilken tid"/>
-                   <button class="bookBtn" onClick={() => this.restaurantBook()}>Skicka</button>
-               </div>
-               <button class="bookButton" onClick={() => store.state.roomModal = true}>Boka Rum</button>
-               <div class="roomModal" v-show={store.state.roomModal}>
-               <button class="closeBtn" onClick={() => store.state.roomModal = false}></button>
-                   Rum
-                   <input class="roomDateArr" type="date" placeholder="Ankomstag"/>
-                   <input class="roomDateDep" type="date" placeholder="När lämnar du oss?"/>
-                   <button class="bookBtn" onClick={() => this.roomBook()}>Skicka</button>
-               </div>
-               <button class="bookButton" onClick={() => store.state.spaModal = true}>Boka Spa</button>
-               <div class="spaModal" v-show={store.state.spaModal}>
-               <button class="closeBtn" onClick={() => store.state.spaModal = false}></button>
-                    Spa
-                    <input class="spaDate" type="date" placeholder="Välj dag"/>
-                   <select class="spaType">
-                       <option class="opt1">Ansikte 80 min</option>
-                       <option class="opt2">Ansikte 50 min</option>
-                       <option class="opt3">Ansikte 20 min</option>
-                       <option class="opt4">Lymfmassage</option>
-                       <option class="opt5">Klassisk</option>
-                       <option class="opt6">Hot stone</option>
-                       <option class="opt7">Aroma anti-stress</option>
-                       <option class="opt8">Gelelackning</option>
-                       <option class="opt9">Manikyr inkl. lackfärg</option>
-                       <option class="opt10">Nagelförstärkning/påfyllning gele</option>
-                       <option class="opt11">Nagelförlängning nytt sätt</option>
-                       <option class="opt12">Kroppskrub</option>
-                       <option class="opt13">Huvudharmoni</option>
-                       <option class="opt14">Fotharmoni</option>
-                       <option class="opt15">Garra ruffa</option>
-                       <option class="opt16">Inpackning</option>
-                   </select>
-                   <button class="bookBtn" onClick={() => this.spaBook()}>Skicka</button>
-                </div> 
-            </div>
+            <>
+                <h1>
+                    Här hittar du alla våra bokningsbara aktiviteter.
+                </h1>
+                <div class="bookingknappar">
+                    <button class="bookButton" onClick={() => store.state.restaurantModal = true}>Boka Resturang</button>
+                    <div class="restaurantModal" v-show={store.state.restaurantModal}>
+                        <button class="closeBtn" onClick={() => store.state.restaurantModal = false}></button>
+                        Resturang
+                        <input class="resDate" type="date" placeholder="Vilken dag?" />
+                        <input class="resTime" type="time" placeholder="Vilken tid" />
+                        <button class="bookBtn" onClick={() => this.restaurantBook()}>Skicka</button>
+                    </div>
+                    <button class="bookButton" onClick={() => store.state.roomModal = true}>Boka Rum</button>
+                    <div class="roomModal" v-show={store.state.roomModal}>
+                        <button class="closeBtn" onClick={() => store.state.roomModal = false}></button>
+                        Rum
+                        <input class="roomDateArr" type="date" placeholder="Ankomstag" />
+                        <input class="roomDateDep" type="date" placeholder="När lämnar du oss?" />
+                        <button class="bookBtn" onClick={() => this.roomBook()}>Skicka</button>
+                    </div>
+                    <button class="bookButton" onClick={() => store.state.spaModal = true}>Boka Spa</button>
+                    <div class="spaModal" v-show={store.state.spaModal}>
+                        <button class="closeBtn" onClick={() => store.state.spaModal = false}></button>
+                        Spa
+                        <input class="spaDate" type="date" placeholder="Välj dag" />
+                        <select class="spaType">
+                            <option class="opt1">Ansikte 80 min</option>
+                            <option class="opt2">Ansikte 50 min</option>
+                            <option class="opt3">Ansikte 20 min</option>
+                            <option class="opt4">Lymfmassage</option>
+                            <option class="opt5">Klassisk</option>
+                            <option class="opt6">Hot stone</option>
+                            <option class="opt7">Aroma anti-stress</option>
+                            <option class="opt8">Gelelackning</option>
+                            <option class="opt9">Manikyr inkl. lackfärg</option>
+                            <option class="opt10">Nagelförstärkning/påfyllning gele</option>
+                            <option class="opt11">Nagelförlängning nytt sätt</option>
+                            <option class="opt12">Kroppskrub</option>
+                            <option class="opt13">Huvudharmoni</option>
+                            <option class="opt14">Fotharmoni</option>
+                            <option class="opt15">Garra ruffa</option>
+                            <option class="opt16">Inpackning</option>
+                        </select>
+                        <button class="bookBtn" onClick={() => this.spaBook()}>Skicka</button>
+                    </div>
+                </div>
+            </>
         )
-       
+
     }
 };
