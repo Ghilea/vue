4aa684dcfda31c46e48447a44fc6253221306996
jojjish <johannab_3@hotmail.com>
2022-05-19 11:48:45 +0100

    Spa

---
+++ src/vue/components/event/dataEvent.js
@@ -0,0 +1,12 @@
+export const spaTreatments = [{
+    title: 'ANSIKTSBEHANDLINGAR',
+    hide: false,
+    treatment: [{
+            name: 'ANSIKTE 80 MIN',
+            describe: 'En rengörande och avslappnande ansiktsbehandling med produkter anpassade efter din hudtyp.I behandlingen ingår lugnande ryggmassage samt att terapeuten skräddarsyr behandlingen efter dina specifika önskemål.',
+            price: 1690,
+            aside: 0,
+            hide: false
+    }]
+}
+]
