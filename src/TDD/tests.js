0eac283284426edf3487427ef844c75a853c749a
Dennis Karlsson <tougent@gmail.com>
2022-05-20 13:16:17 +0200

    update

---
+++ src/TDD/tests.js
@@ -0,0 +1,16 @@
+const chai = window.chai
+const expect = chai.expect
+
+describe('getCelcius', () => {
+    it('Ska göra om farenheit till celcius från en array med värden', () => {
+        expect(getCelcius([23, 140, 212, 41])).to.deep.equal([-5, 60, 100, 5])
+        
+    })
+}) 
+
+describe('currentTime', () => {
+    it('Ska visa dagens datum', () => {
+        expect(currentTime()).to.deep.equal('20/5/2022')
+
+    })
+})
